1eaf6fcc58f995396fed35ce031cd336c02a607d
Magomedov Arthur <artur22245@gmail.com>
2022-04-05 17:04:26 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rs1 = (height - 100) * 1.15;
+        return rs1;
+    }
+
+    public static double womanWeight(short height) {
+        double rs1 = (height - 110) * 1.15;
+        return rs1;
+    }
+
+    public static void main(String[] args) {
+        short height = 177;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 177 is " + man);
+
+        height = 160;
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman 160 is " + woman);
+    }
+}
