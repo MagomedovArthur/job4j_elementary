a418ccbde7b5a0b27da060b18c9fcbfec2b67b86
Magomedov Arthur <artur22245@gmail.com>
2022-04-09 22:23:58 +0300

    4.1.2. Глупый бот [#31323 #286510]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,13 +3,13 @@ package ru.job4j.condition;
 public class DummyBot {
 
     public static String answer(String question) {
-        String result = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            result = "Привет, умник.";
+            rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
-            result = "До скорой встречи.";
+            rsl = "До скорой встречи.";
         }
-        return result;
+        return rsl;
     }
 
     public static void main(String[] args) {
