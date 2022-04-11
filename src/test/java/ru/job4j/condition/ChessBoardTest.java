9ccd4869614b8d0cce43ab09b25e4eef2d2ae02e
Magomedov Arthur <artur22245@gmail.com>
2022-04-11 16:41:24 +0300

    4.3.6. Math.abs [#373616]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -36,7 +36,7 @@ public class ChessBoardTest {
         int x2 = 4;
         int y2 = 1;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
+        int expected = 2;
         Assert.assertEquals(expected, result);
     }
 
