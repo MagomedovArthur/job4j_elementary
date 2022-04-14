e5eed78dd9899cddfa46e1758af001da287d5a54
Magomedov Arthur <artur22245@gmail.com>
2022-04-14 18:55:35 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;          //сумма кредита  //доход      // % ставка
+        while (amount > 0) {
+            percent = percent / 100;
+            double credit = amount + amount * percent;
+            amount = credit - salary;
+            year++;
+        }
+
+        return year;
+    }
+}
