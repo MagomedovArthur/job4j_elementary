1db4ed393435c4a814078555470d18da6ae162f9
Magomedov Arthur <artur22245@gmail.com>
2022-04-14 18:09:01 +0300

    5.4. Протеиновая диета [#156313 #288488]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -6,11 +6,7 @@ public class Fitness {
         while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
-            if (ivan > nik) {
-                month++;
-            } else {
-                month++;
-            }
+            month++;
         }
         return month;
     }
