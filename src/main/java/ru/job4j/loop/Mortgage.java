2a92ed21d346b13ec53de66b02de027fec058323
Magomedov Arthur <artur22245@gmail.com>
2022-04-16 12:19:07 +0300

    5.7. Ипотека [#156315 #288971]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
+        percent = percent / 100;
         while (amount > 0) {
-            percent = percent / 100;
             amount = (amount + amount * percent) - salary;
             year++;
         }
